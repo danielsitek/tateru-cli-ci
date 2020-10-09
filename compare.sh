e8bf715db9102c991430875c41f5d82a5b02e14d
Daniel Sitek <dan.sitek@gmail.com>
2020-10-09 09:25:35 +0200

    dev and prod samples for diff testing

--- compare.sh
+++ compare.sh
@@ -7,29 +7,31 @@
 # @link https://www.linuxquestions.org/questions/linux-software-2/diff-how-do-i-read-it-as-true-or-false-871240/
 # ###
 
+ENV=${1:-dev}
+
 testDiff() {
     RETURN=0;
 
-    printf "\n";
-    printf "Testing Diff\n";
-    echo "------------";
-    printf "\n";
+    printf "%s\n" "";
+    printf "%s\n" "${ENV^^} Testing Diff";
+    printf "%s\n" "-----------------";
+    printf "%s\n" "";
 
     for file in "$@";
     do
-        SAMPLE_FILE="sample/${file}";
+        SAMPLE_FILE="sample/${ENV}/${file}";
         DIST_FILE="dist/${file}";
         EXISTS=0
 
         if [ ! -e $SAMPLE_FILE ]
         then
-            printf "File sample/${file} does not exists\n";
+            printf "%s\n" "File sample/${ENV}/${file} does not exists";
             EXISTS=1
         fi
 
         if [ ! -e $DIST_FILE ]
         then
-            printf "File dist/${file} does not exists\n";
+            printf "%s\n" "File dist/${file} does not exists";
             EXISTS=1
         fi
 
@@ -38,9 +40,9 @@ testDiff() {
             diff -q $SAMPLE_FILE $DIST_FILE 1>/dev/null
             if [ $? -eq 0 ]
             then
-                printf "> %-30s same\n" ${file};
+                printf "> %-30s same\n" "${file}";
             else
-                printf "> %-30s diff\n" ${file};
+                printf "> %-30s diff\n" "${file}";
                 RETURN=1
             fi
         else
@@ -48,8 +50,8 @@ testDiff() {
         fi
     done
 
-    printf "\n"
-    printf "Result >"
+    printf "%s\n" ""
+    printf "%s" "Result >"
 
     if [ $RETURN -eq 0 ]
     then
@@ -58,9 +60,11 @@ testDiff() {
         printf "%-24s \e[1;31mFiles are in diff\e[0m\n";
     fi
 
+    printf "%s\n" ""
+
     return $RETURN
 }
 
-testDiff 'index.html' 'about.html' 'robots.txt'
+testDiff 'index.html' 'about.html' 'robots.txt' 'sitemap.xml' 'manifest.json'
 
 exit $?;
